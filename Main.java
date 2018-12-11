89f1c344b5b35bbc93e995579d47a6e5ade80b3c
hagelcha <43893556+hagelcha@users.noreply.github.com>
2018-12-10 23:00:41 -0600

    Updated the list of imports at the top, some were missing

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+package application;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -6,55 +7,34 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-
 import javafx.event.EventHandler;
-
 import javafx.geometry.Insets;
-
 import javafx.geometry.Pos;
-
 import javafx.stage.Stage;
-
 import javafx.scene.Group;
-
 import javafx.scene.Scene;
-
 import javafx.scene.control.Button;
-
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TableColumn;
-
 import javafx.scene.control.TableView;
-
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
-
 import javafx.scene.layout.GridPane;
-
 import javafx.scene.layout.HBox;
-
 import javafx.scene.layout.Pane;
-
 import javafx.scene.layout.VBox;
-
 import javafx.scene.paint.Color;
-
 import javafx.scene.text.Font;
-
 import javafx.scene.text.FontWeight;
-
 import javafx.scene.text.Text;
-
 import javafx.geometry.HPos;
-
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.*;
 
